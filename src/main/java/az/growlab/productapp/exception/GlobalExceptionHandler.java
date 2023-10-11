efe98c10cbf69a1b0f12bef6b483fb365c51cf73
Nabiyeff <101498745+Nabiyeff@users.noreply.github.com>
2023-10-11 15:51:31 +0400

    Exception Handler changed

--- src/main/java/az/growlab/productapp/exception/GlobalExceptionHandler.java
+++ src/main/java/az/growlab/productapp/exception/GlobalExceptionHandler.java
@@ -4,16 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleValidationException(MethodArgumentNotValidException exception) {
         Map<String, String> errorMap = new HashMap<>();
         exception.getBindingResult().getAllErrors().forEach(error -> {
@@ -26,7 +27,7 @@ public class ExceptionHandler {
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @org.springframework.web.bind.annotation.ExceptionHandler(CustomNotFoundException.class)
+    @ExceptionHandler(CustomNotFoundException.class)
     public ResponseEntity<String> handleNotFoundException(CustomNotFoundException exception){
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
